2940f288e70df4924ec1ef21c220c85da0f5c6ea
David Brás <48626442+Savider@users.noreply.github.com>
2020-11-29 15:56:10 +0000

    added a simple template

--- website/matchcore/views.py
+++ website/matchcore/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import Project
 
 
 def project_page(request, project_id):
-    response = "You're looking at project %s."
-    return HttpResponse(response % project_id)
+    current_project = Project.objects.get(id=project_id)
+    context = {
+        'current_project': current_project,
+    }
+    return render(request, 'matchcore/project_page.html', context)
 
 
 def project(request):
     project_list = Project.objects.all()
-    output = ', '.join([p.title for p in project_list])
+    output = ', '.join([p.id.__str__() for p in project_list])
     return HttpResponse(output)
