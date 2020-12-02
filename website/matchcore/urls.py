cf1f07d73a5cbf689d280fcebe75a3a0a5a13baa
David Brás <48626442+Savider@users.noreply.github.com>
2020-12-02 12:44:06 +0000

    a small change

--- website/matchcore/urls.py
+++ website/matchcore/urls.py
@@ -6,10 +6,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.login_page, name='login_page'),
-    path('project/', views.project, name='project'),
-    path('project/<int:project_id>/', views.project_page, name='project_page'),
-    path('user/<str:username>/', views.user_page, name='user_page'),
+    path('', views.login_page),
+    path('project/', views.project),
+    path('project/<int:project_id>/', views.project_page),
+    path('user/<str:username>/', views.user_page),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
